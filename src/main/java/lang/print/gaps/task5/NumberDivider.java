032d43a5d7e81525c3f7d32636204756e72e21ba
Ali Orynbasar <96694797+ssendamevil@users.noreply.github.com>
2023-10-10 21:19:42 +0600

    Solved 1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        System.out.println(toBeDivided / 5);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider numberDivider = new NumberDivider();
+        numberDivider.divide(15);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        System.out.println(minutes * 60);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor timeConvertor = new TimeConvertor();
+        timeConvertor.convert(2F);
     }
 }
